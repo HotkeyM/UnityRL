60e760621e27136f62ffcdbe5519ca3c9b3b4e2d
HotkeyM <hotkeym@gmail.com>
2015-09-10 13:48:43 +0300

    level gen and turns
    level generation realized with turn-based square cell movement prototype

---
+++ assets/Anima.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anima : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Animator an = GetComponent<Animator>();
+		an.CrossFade("WalkFwdLoop",0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
